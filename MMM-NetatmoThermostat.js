3287dcb193864a52d727266445cc5ac930aaed11
bugsounet <github@bugsounet.fr>
2024-04-25 07:59:25 +0200

    work save

---
+++ MMM-NetatmoThermostat.js
@@ -0,0 +1,245 @@
+/*
+ * Module: MMM-NetatmoThermostat
+ *
+ * By bugsounet ©2024
+ * MIT Licensed.
+ */
+logNT = (...args) => { /* do nothing */ };
+
+Module.register("MMM-NetatmoThermostat", {
+  requiresVersion: "2.26.0",
+  defaults: {
+    debug: false,
+    verbose: false,
+    updateInterval: 30000,
+    home_id: null,
+    room_id: 0,
+    api: {
+      client_id: null,
+      client_secret: null,
+      access_token: null,
+      refresh_token: null
+    },
+    display: {
+      name: true,
+      mode: true,
+      battery: true,
+      firmware: true,
+      signal: true,
+      tendency: true
+    }
+  },
+
+  start () {
+    this.Thermostat = null;
+  },
+
+  getDom () {
+    var wrapper = document.createElement("div");
+    wrapper.id = "NETATMO";
+
+    var temp = document.createElement("div");
+    temp.id = "NETATMO_TEMP";
+    var zone1 = document.createElement("div");
+    zone1.id = "NETATMO_ZONE1";
+
+    var tempset = document.createElement("div");
+    tempset.id = "NETATMO_TEMPSET";
+    var tempsetIcon = document.createElement("div");
+    tempsetIcon.id= "NETATMO_TEMPSET_ICON";
+    tempset.appendChild(tempsetIcon);
+    var tempsetValue = document.createElement("div");
+    tempsetValue.id= "NETATMO_TEMPSET_VALUE";
+    tempset.appendChild(tempsetValue);
+    if (!this.config.display.mode) {
+      tempsetIcon.className = "hidden";
+      tempsetValue.className = "hidden";
+    }
+    zone1.appendChild(tempset);
+
+    var firmware = document.createElement("div");
+    firmware.id = "NETATMO_FIRMWARE";
+    var firmwareIcon = document.createElement("div");
+    firmwareIcon.id = "NETATMO_FIRMWARE_ICON";
+    firmware.appendChild(firmwareIcon);
+    var firmwareValue = document.createElement("div");
+    firmwareValue.id = "NETATMO_FIRMWARE_VALUE";
+    firmware.appendChild(firmwareValue);
+    if (!this.config.display.firmware) {
+      firmwareIcon.className = "hidden";
+      firmwareValue.className = "hidden";
+    }
+    zone1.appendChild(firmware);
+
+    temp.appendChild(zone1);
+
+    var zone2 = document.createElement("div");
+    zone2.id = "NETATMO_ZONE2";
+
+    var name = document.createElement("div");
+    name.id = "NETATMO_NAME";
+    zone2.appendChild(name);
+
+    var tempValue = document.createElement("div");
+    tempValue.id = "NETATMO_TEMP_VALUE";
+    tempValue.innerHTML = "<img src ='modules/MMM-NetatmoThermostat/resources/netatmo-logo.png' style='width: 200px;'>";
+    zone2.appendChild(tempValue);
+
+    var empty = document.createElement("div");
+    empty.id = "NETATMO_EMPTY";
+    zone2.appendChild(empty);
+
+    temp.appendChild(zone2);
+
+    var zone3 = document.createElement("div");
+    zone3.id = "NETATMO_ZONE3";
+
+    var battery = document.createElement("div");
+    battery.id = "NETATMO_BATTERY";
+    var batteryIcon = document.createElement("div");
+    batteryIcon.id = "NETATMO_BATTERY_ICON";
+    battery.appendChild(batteryIcon);
+    var batteryValue = document.createElement("div");
+    batteryValue.id = "NETATMO_BATTERY_VALUE";
+    battery.appendChild(batteryValue);
+    if (!this.config.display.battery) {
+      batteryIcon.className = "hidden";
+      batteryValue.className = "hidden";
+    }
+    zone3.appendChild(battery);
+
+    var tempTendency = document.createElement("div");
+    tempTendency.id = "NETATMO_TEMP_TENDENCY";
+    var tempTendencyIcon = document.createElement("div");
+    tempTendencyIcon.id = "NETATMO_TEMP_TENDENCY_ICON";
+    tempTendency.appendChild(tempTendencyIcon);
+    if (!this.config.display.tendency) tempTendencyIcon.className = "hidden";
+    zone3.appendChild(tempTendency);
+
+    var signal = document.createElement("div");
+    signal.id = "NETATMO_RADIO";
+    var signalIcon = document.createElement("div");
+    signalIcon.id = "NETATMO_RADIO_ICON";
+    signal.appendChild(signalIcon);
+    var signalValue = document.createElement("div");
+    signalValue.id = "NETATMO_RADIO_VALUE";
+    signal.appendChild(signalValue);
+    if (!this.config.display.signal) {
+      signalIcon.className = "hidden";
+      signalValue.className = "hidden";
+    }
+    zone3.appendChild(signal);
+
+    temp.appendChild(zone3);
+    wrapper.appendChild(temp);
+
+    return wrapper;
+  },
+
+  getStyles () {
+    return [
+      "MMM-NetatmoThermostat.css",
+      "font-awesome.css"
+    ];
+  },
+
+  socketNotificationReceived (noti, payload) {
+    switch(noti) {
+      case "DATA":
+        this.updateData(payload);
+        break;
+    }
+  },
+
+  notificationReceived (noti, payload) {
+    switch(noti) {
+      case "DOM_OBJECTS_CREATED":
+        if (this.config.debug) logNT = (...args) => { console.log("[NETATMO]", ...args); };
+        if (this.config.updateInterval < 30000) this.config.updateInterval = 30000;
+        this.sendSocketNotification("INIT", this.config);
+        break;
+    }
+  },
+
+  updateData (data) {
+    if (!data) return console.error("[NETATMO] No Data!");
+    this.Thermostat = data;
+    logNT(this.Thermostat);
+
+    var name = document.getElementById("NETATMO_NAME");
+    var batteryIcon = document.getElementById("NETATMO_BATTERY_ICON");
+    var batteryValue = document.getElementById("NETATMO_BATTERY_VALUE");
+    var signalIcon = document.getElementById("NETATMO_RADIO_ICON");
+    var signalValue = document.getElementById("NETATMO_RADIO_VALUE");
+    var tempTendencyIcon = document.getElementById("NETATMO_TEMP_TENDENCY_ICON");
+    var temp = document.getElementById("NETATMO_TEMP");
+    var tempValue = document.getElementById("NETATMO_TEMP_VALUE");
+    var tempsetValue = document.getElementById("NETATMO_TEMPSET_VALUE");
+    var tempsetIcon = document.getElementById("NETATMO_TEMPSET_ICON");
+    var firmwareIcon = document.getElementById("NETATMO_FIRMWARE_ICON");
+    var firmwareValue = document.getElementById("NETATMO_FIRMWARE_VALUE");
+
+    if (this.config.display.name) name.textContent = this.Thermostat.name;
+
+    tempValue.textContent = `${this.Thermostat.temp.toFixed(1)}°`;
+    if (this.config.display.tendency) {
+      tempTendencyIcon.className= this.tempTendency(this.Thermostat.tendency);
+    }
+
+    if (this.config.display.mode) {
+      if ((this.Thermostat.mode === "schedule") || (this.Thermostat.mode === "manual")) {
+        tempsetValue.textContent = `${this.Thermostat.tempSet}°`;
+      }
+      switch (this.Thermostat.mode) {
+        case "schedule":
+          tempsetIcon.className = "far fa-calendar-check";
+          break;
+        case "manual":
+          tempsetIcon.className = "fas fa-hand-paper";
+          break;
+        case "off":
+          tempsetIcon.className = "fas fa-power-off";
+          tempsetValue.textContent = "OFF";
+          break;
+        case "max":
+          tempsetIcon.className = "fas fa-thermometer-full";
+          tempsetValue.textContent = "MAX";
+          break;
+      }
+    }
+
+    if (this.config.display.firmware) {
+      firmwareIcon.className= "fas fa-microchip";
+      firmwareValue.textContent = this.Thermostat.firmware;
+    }
+
+    if (this.config.display.signal) {
+      signalIcon.className= "fas fa-signal";
+      signalValue.textContent = `${this.Thermostat.signal}%`;
+    }
+
+    if (this.config.display.battery) {
+      batteryIcon.className = this.Thermostat.battery > 95
+        ? "fa fa-battery-full"
+        : this.Thermostat.battery >= 70
+          ? "fa fa-battery-three-quarters"
+          : this.Thermostat.battery >= 45
+            ? "fa fa-battery-half"
+            : this.Thermostat.battery >= 15
+              ? "fa fa-battery-quarter"
+              : "fa fa-battery-empty";
+      batteryValue.textContent = `${this.Thermostat.battery}%`;
+    }
+
+    if (this.Thermostat.heating) temp.className = "heating";
+    else temp.classList.remove("heating");
+  },
+
+  tempTendency (tendency) {
+    let icon;
+    if (tendency === 1 ) icon = "fa fa-caret-up";
+    else if (tendency === 2) icon = "fa fa-caret-down";
+    else icon = "fa fa-caret-right";
+    return icon;
+  }
+});
--- node_helper.js
+++ node_helper.js
@@ -1,14 +1,14 @@
 /*
- * Node Helper: MMM-Netatmo-Thermostat
+ * Node Helper: MMM-NetatmoThermostat
  *
  * By bugsounet ©2024
  * MIT Licensed.
  */
 
-var NodeHelper = require("node_helper")
-var netatmo = require('./netatmo')
+var NodeHelper = require("node_helper");
+var netatmo = require("./components/netatmo.js");
 
-logNT = (...args) => { /* do nothing ! */ }
+logNT = (...args) => { /* do nothing ! */ };
 
 module.exports = NodeHelper.create({
   start () {
@@ -24,109 +24,109 @@ module.exports = NodeHelper.create({
       mode: null,
       signal: null,
       firmware: null
-    }
+    };
     this.tempHistory = {
       data: [],
       average: null,
       lastAverage: null
-    }
-    this.Authenticated = false
-    this.api = null
+    };
+    this.Authenticated = false;
+    this.api = null;
   },
 
   socketNotificationReceived (noti, payload) {
     switch (noti) {
       case "INIT":
-        console.log("[NETATMO] MMM-Netatmo-Thermostat Version:", require('./package.json').version)
-        this.initialize(payload)
-      break
+        console.log("[NETATMO] MMM-NetatmoThermostat Version:", require("./package.json").version, "rev:", require("./package.json").rev);
+        this.initialize(payload);
+        break;
     }
   },
 
   initialize (config) {
-    this.config = config
-    if (this.config.debug) logNT = (...args) => { console.log("[NETATMO]", ...args) }
-    if (!this.config.home_id) return console.error("[NETATMO] home_id not set!")
-    console.log("[NETATMO] Starting MMM-Netatmo-Thermostat module...")
-    logNT(this.config)
-    this.api = new netatmo(this.config.api)
+    this.config = config;
+    if (this.config.debug) logNT = (...args) => { console.log("[NETATMO]", ...args); };
+    if (!this.config.home_id) return console.error("[NETATMO] home_id not set!");
+    console.log("[NETATMO] Starting MMM-NetatmoThermostat module...");
+    logNT(this.config);
+    this.api = new netatmo(this.config.api);
     this.api
-      .on('get-thermostatsdata', (err, devices) => this.getThermostatsData(err, devices))
-      .on('get-homestatus', (err, data) => this.getHomeStatus(err, data))
-      .on("error", error => {
-        console.error('[NETATMO] threw an error: ' + error)
+      .on("get-thermostatsdata", (err, devices) => this.getThermostatsData(err, devices))
+      .on("get-homestatus", (err, data) => this.getHomeStatus(err, data))
+      .on("error", (error) => {
+        console.error(`[NETATMO] threw an error: ${  error}`);
       })
-      .on("warning", error => {
-        console.log('[NETATMO] threw a warning: ' + error)
+      .on("warning", (error) => {
+        console.log(`[NETATMO] threw a warning: ${  error}`);
       })
       .on("authenticated", () => {
-        console.log("[NETATMO] Authenticated!")
-        this.Authenticated = true
-      })
+        console.log("[NETATMO] Authenticated!");
+        this.Authenticated = true;
+      });
 
-    this.api.homeStatus({ "home_id": this.config.home_id })
+    this.api.homeStatus({ home_id: this.config.home_id });
 
     setInterval(()=> {
       if (this.Authenticated) { // auth only ?
-        logNT("Updating...")
-        this.api.homeStatus({ "home_id": this.config.home_id })
+        logNT("Updating...");
+        this.api.homeStatus({ home_id: this.config.home_id });
       }
-    }, this.config.updateInterval)
+    }, this.config.updateInterval);
   },
 
   getThermostatsData (err, devices) {
-    logNT("ThermostatsData:", devices)
-    devices.forEach(device => {
-      if (device._id == this.thermostat.bridge) { // select Relay
-        device.modules.forEach(moduleData => {
-          if (moduleData._id == this.thermostat.id) { // select Thermostat
-            this.thermostat.battery = moduleData.battery_percent
-            this.thermostat.name = moduleData.module_name
+    if (this.config.verbose) logNT("ThermostatsData:", devices);
+    devices.forEach((device) => {
+      if (device._id === this.thermostat.bridge) { // select Relay
+        device.modules.forEach((moduleData) => {
+          if (moduleData._id === this.thermostat.id) { // select Thermostat
+            this.thermostat.battery = moduleData.battery_percent;
+            this.thermostat.name = moduleData.module_name;
           }
-        })
+        });
       }
-    })
-    logNT("Final Result:", this.thermostat)
-    this.sendSocketNotification("DATA", this.thermostat)
+    });
+    logNT("Final Result:", this.thermostat);
+    this.sendSocketNotification("DATA", this.thermostat);
   },
 
   getHomeStatus (err, data) {
-    logNT("HomeStatus:", data.home)
+    if (this.config.verbose) logNT("HomeStatus:", data.home);
     if (data.home.rooms.length && data.home.rooms.length-1 >= this.config.room_id) {
-      this.thermostat.temp = data.home.rooms[this.config.room_id].therm_measured_temperature
-      this.thermostat.tempSet = data.home.rooms[this.config.room_id].therm_setpoint_temperature
-      this.thermostat.mode = data.home.rooms[this.config.room_id].therm_setpoint_mode
-      this.thermostat.tendency = this.averageTemp(this.thermostat.temp)
-      data.home.modules.forEach(module => {
-        if (module.type == "NATherm1") {
-          this.thermostat.bridge = module.bridge
-          this.thermostat.id = module.id
-          this.thermostat.heating = module.boiler_status
-          this.thermostat.firmware = module.firmware_revision
-          this.thermostat.signal = module.rf_strength
+      this.thermostat.temp = data.home.rooms[this.config.room_id].therm_measured_temperature;
+      this.thermostat.tempSet = data.home.rooms[this.config.room_id].therm_setpoint_temperature;
+      this.thermostat.mode = data.home.rooms[this.config.room_id].therm_setpoint_mode;
+      this.thermostat.tendency = this.averageTemp(this.thermostat.temp);
+      data.home.modules.forEach((module) => {
+        if (module.type === "NATherm1") {
+          this.thermostat.bridge = module.bridge;
+          this.thermostat.id = module.id;
+          this.thermostat.heating = module.boiler_status;
+          this.thermostat.firmware = module.firmware_revision;
+          this.thermostat.signal = module.rf_strength;
         }
-      })
-    } else console.error("[NETATMO] no rooms found!", data.home)
-    this.api.getThermostatsData()
+      });
+    } else console.error("[NETATMO] no rooms found!", data.home);
+    this.api.getThermostatsData();
   },
 
   averageTemp (temp) {
-    if (!temp) return
-    let average = 0
+    if (!temp) return;
+    let average = 0;
     /** do Array of last 10 Temp **/
-    if (this.tempHistory.data.length >= 10) this.tempHistory.data.splice(0,1)
-    this.tempHistory.data.push(temp)
+    if (this.tempHistory.data.length >= 10) this.tempHistory.data.splice(0,1);
+    this.tempHistory.data.push(temp);
 
     /** do the average **/
-    this.tempHistory.data.forEach(value => {
-      average += value
-    })
-    average = (average/this.tempHistory.data.length).toFixed(2)
-    this.tempHistory.lastAverage = this.tempHistory.average ? this.tempHistory.average: average
-    this.tempHistory.average= average
-    logNT("tempHistory:", this.tempHistory)
-    if (this.tempHistory.average > this.tempHistory.lastAverage) return 1
-    if (this.tempHistory.average < this.tempHistory.lastAverage) return 2
-    return 0
+    this.tempHistory.data.forEach((value) => {
+      average += value;
+    });
+    average = (average/this.tempHistory.data.length).toFixed(2);
+    this.tempHistory.lastAverage = this.tempHistory.average ? this.tempHistory.average: average;
+    this.tempHistory.average= average;
+    logNT("tempHistory:", this.tempHistory);
+    if (this.tempHistory.average > this.tempHistory.lastAverage) return 1;
+    if (this.tempHistory.average < this.tempHistory.lastAverage) return 2;
+    return 0;
   }
 });
