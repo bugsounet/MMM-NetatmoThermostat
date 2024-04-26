82d4cb4fbe01600285531f610fdcff6a7a590578
bugsounet <github@bugsounet.fr>
2024-04-26 16:19:07 +0200

    recode library with node fetch

--- components/netatmo.js
+++ components/netatmo.js
@@ -8,7 +8,6 @@
 
 var util = require("util");
 var EventEmitter = require("events").EventEmitter;
-var request = require("request");
 
 const BASE_URL = "https://api.netatmo.net";
 
@@ -29,7 +28,7 @@ var netatmo = function (args) {
 util.inherits(netatmo, EventEmitter);
 
 /**
- * handleRequestError
+ * handleFetchError
  * @param err
  * @param response
  * @param body
@@ -37,15 +36,14 @@ util.inherits(netatmo, EventEmitter);
  * @param critical
  * @returns {Error}
  */
-netatmo.prototype.handleRequestError = function (err, response, body, message, critical) {
+netatmo.prototype.handleFetchError = function (err, message, critical) {
   var errorMessage = "";
-  if (body && response.headers["content-type"].trim().toLowerCase().indexOf("application/json") !== -1) {
-    errorMessage = JSON.parse(body);
-    errorMessage = errorMessage && (errorMessage.error.message || errorMessage.error);
-  } else if (typeof response !== "undefined") {
-    errorMessage = `Status code ${response.statusCode}`;
+  if (typeof (err) === "object") {
+    if (err.error && err.error_description) errorMessage = `${err.error} (${err.error_description})`;
+    else if (err.cause) errorMessage = `${err.message} (${err.cause})`;
+    else  errorMessage = err.toString();
   } else {
-    errorMessage = "No response";
+    errorMessage = "No error response -- Received: $err";
   }
   var error = new Error(`${message}: ${errorMessage}`);
   if (critical) {
@@ -83,45 +81,47 @@ netatmo.prototype.authenticate = function (args, callback) {
     return this;
   }
 
-
   client_id = args.client_id;
   client_secret = args.client_secret;
   refresh_token = args.refresh_token;
 
-  var form = {
-    grant_type: "refresh_token",
-    refresh_token: refresh_token,
-    client_id: client_id,
-    client_secret: client_secret
-  };
+  const params = new URLSearchParams();
+  params.append("grant_type", "refresh_token");
+  params.append("refresh_token", refresh_token);
+  params.append("client_id", client_id);
+  params.append("client_secret", client_secret);
 
   var url = util.format("%s/oauth2/token", BASE_URL);
-
-  request({
-    url: url,
+  var fetchOptions = {
     method: "POST",
-    form: form
-  }, function (err, response, body) {
-    if (err || response.statusCode !== 200) {
-      return this.handleRequestError(err, response, body, "Authenticate error", true);
-    }
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params
+  };
 
-    body = JSON.parse(body);
+  fetch(url, fetchOptions)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.error) {
+        return this.handleFetchError(data, "Authenticate error", true);
+      }
+      access_token = data.access_token;
 
-    access_token = body.access_token;
+      if (data.expires_in) {
+        setTimeout(this.authenticate_refresh.bind(this), data.expires_in * 1000, data.refresh_token);
+      }
 
-    if (body.expires_in) {
-      setTimeout(this.authenticate_refresh.bind(this), body.expires_in * 1000, body.refresh_token);
-    }
+      this.emit("authenticated", data.expires_in);
 
-    this.emit("authenticated", body.expires_in);
-
-    if (callback) {
-      return callback();
-    }
-
-    return this;
-  }.bind(this));
+      if (callback) {
+        return callback();
+      }
+      return this;
+    })
+    .catch ((error) => {
+      return this.handleFetchError(error, "Authenticate error", true);
+    });
 
   return this;
 };
@@ -133,36 +133,41 @@ netatmo.prototype.authenticate = function (args, callback) {
  */
 netatmo.prototype.authenticate_refresh = function (refresh_token) {
 
-  var form = {
-    grant_type: "refresh_token",
-    refresh_token: refresh_token,
-    client_id: client_id,
-    client_secret: client_secret
+  const params = new URLSearchParams();
+  params.append("grant_type", "refresh_token");
+  params.append("refresh_token", refresh_token);
+  params.append("client_id", client_id);
+  params.append("client_secret", client_secret);
+
+  var fetchOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params
   };
 
   var url = util.format("%s/oauth2/token", BASE_URL);
 
-  request({
-    url: url,
-    method: "POST",
-    form: form
-  }, function (err, response, body) {
-    if (err || response.statusCode !== 200) {
-      return this.handleRequestError(err, response, body, "Authenticate refresh error");
-    }
+  fetch(url, fetchOptions)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.error) {
+        return this.handleFetchError(data, "Authenticate error", true);
+      }
+      access_token = data.access_token;
 
-    body = JSON.parse(body);
-
-    access_token = body.access_token;
-
-    if (body.expires_in) {
-      setTimeout(this.authenticate_refresh.bind(this), body.expires_in * 1000, body.refresh_token);
-    }
+      if (data.expires_in) {
+        setTimeout(this.authenticate_refresh.bind(this), data.expires_in * 1000, data.refresh_token);
+      }
     
-    this.emit("refreshed", body.expires_in);
+      this.emit("refreshed", data.expires_in);
 
-    return this;
-  }.bind(this));
+      return this;
+    })
+    .catch ((error) => {
+      return this.handleFetchError(error, "Authenticate refresh error");
+    });
 
   return this;
 };
@@ -183,35 +188,40 @@ netatmo.prototype.getHomesData = function (options, callback) {
 
   var url = util.format("%s/api/homesdata", BASE_URL);
 
-  var form = {
-    access_token: access_token
-  };
+  const params = new URLSearchParams();
+  params.append("access_token", access_token);
 
   if (options) {
-    if (options.home_id) form.home_id = options.home_id;
-    if (options.gateway_types) form.gateway_types = options.gateway_types;
+    if (options.home_id) params.append("home_id", options.home_id);
+    if (options.gateway_types) params.append("gateway_types", options.gateway_types);
   }
 
-  request({
-    url: url,
+  var fetchOptions = {
     method: "POST",
-    form: form
-  }, function (err, response, body) {
-    if (err || response.statusCode !== 200) {
-      return this.handleRequestError(err, response, body, "getHomesData error");
-    }
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params
+  };
 
-    body = JSON.parse(body);
+  fetch(url, fetchOptions)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.error) {
+        return this.handleFetchError(data, "getHomesData error");
+      }
 
-    this.emit("get-homesdata", err, body.body);
+      this.emit("get-homesdata", data.error, data.body);
 
-    if (callback) {
-      return callback(err, body.body);
-    }
+      if (callback) {
+        return callback(data.error, body.body);
+      }
 
-    return this;
-
-  }.bind(this));
+      return this;
+    })
+    .catch ((error) => {
+      return this.handleFetchError(error, "getHomesData error");
+    });
 
   return this;
 };
@@ -242,37 +252,42 @@ netatmo.prototype.homeStatus = function (options, callback) {
 
   var url = util.format("%s/api/homestatus", BASE_URL);
 
-  var form = {
-    access_token: access_token,
-    home_id: options.home_id
-  };
+  const params = new URLSearchParams();
+  params.append("access_token", access_token);
+  params.append("home_id", options.home_id);
 
   if (options) {
     if (options.device_types) {
-      form.device_types = options.device_types;
+      params.append("device_typed", options.device_types);
     }
   }
- 
-  request({
-    url: url,
+
+  var fetchOptions = {
     method: "POST",
-    form: form
-  }, function (err, response, body) {
-    if (err || response.statusCode !== 200) {
-      return this.handleRequestError(err, response, body, "homeStatus error");
-    }
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params
+  };
 
-    body = JSON.parse(body);
+  fetch(url, fetchOptions)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.error) {
+        return this.handleFetchError(data, "homeStatus error");
+      }
 
-    this.emit("get-homestatus", err, body.body);
+      this.emit("get-homestatus", data.error, data.body);
 
-    if (callback) {
-      return callback(err, body.body);
-    }
+      if (callback) {
+        return callback(data.error, data.body);
+      }
 
-    return this;
-
-  }.bind(this));
+      return this;
+    })
+    .catch ((error) => {
+      return this.handleFetchError(error, "homeStatus error");
+    });
 
   return this;
 };
